a8f774aabdeda5ff88a9b570438b227bba080677
Noobmaster169 <mariotaning@gmail.com>
2024-09-29 02:02:54 +0800

    Add Web3 Login Button

--- optifirm_ai/app/page.tsx
+++ optifirm_ai/app/page.tsx
@@ -1,8 +1,13 @@
+"use client";
 import Image from "next/image";
+import {useState} from "react";
+import { useReadContract, useSendTransaction, useWriteContract } from 'wagmi'
+import ConnectButton from '@/components/ConnectButton';
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <ConnectButton/>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
---
+++ optifirm_ai/components/ConnectButton.tsx
@@ -0,0 +1,3 @@
+export default function ConnectButton() {
+    return(<div><w3m-button size={"md"} balance={"hide"} label={"Log In"} loadingLabel={"Logging In"}/></div>)
+}
