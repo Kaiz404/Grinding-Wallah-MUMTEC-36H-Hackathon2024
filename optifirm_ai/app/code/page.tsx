97170f6ca2d36fcca29d504a056ec714dc01f14b
Skooy <104994184+Skooyo@users.noreply.github.com>
2024-09-29 08:17:20 +0800

    added test case page

--- optifirm_ai/app/code/page.tsx
+++ optifirm_ai/app/code/page.tsx
@@ -2,7 +2,6 @@
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { darcula } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
 import { useState, useEffect } from 'react';
-import Link from 'next/link';
 
 export default function Home() {
 
@@ -29,7 +28,26 @@ export default function Home() {
     <div className="h-full w-full flex py-8 items-center justify-center gap-32">
 
       <div className="flex-col flex w-1/3 h-full border border-slate-500 rounded-lg items-center">
-        <p className="text-lg font-semibold text-white">Generated Test Cases</p>
+        <p className="text-lg font-semibold text-white">Old Code</p>
+        <div className="flex w-full h-full overflow-auto">
+            <SyntaxHighlighter 
+            language="python" 
+            style={darcula} 
+            customStyle={{
+            width: "100%", 
+            maxHeight: "100%", 
+            overflow: "auto", 
+            whiteSpace: "pre-wrap",
+            fontSize: "1.2em"
+            }}
+            >
+            {code.join("\n")}
+            </SyntaxHighlighter>
+        </div>
+      </div>
+
+      <div className="flex-col flex w-1/3 h-full border border-slate-500 rounded-lg items-center">
+        <p className="text-lg font-semibold text-white">New Code</p>
         <div className="flex w-full h-full overflow-auto">
         <SyntaxHighlighter 
             language="python" 
@@ -45,26 +63,22 @@ export default function Home() {
             {currentText}
             </SyntaxHighlighter>
         </div>
-      </div>
 
-        <div className="flex h-full justify-center items-center gap-16 flex-col">
-            <button 
-            className='rounded-3xl py-4 text-2xl p-8 bg-red-500 text-white items-center flex justify-center'
-            onClick={(e) => {
-            e.preventDefault();
-            window.location.reload();
-            }}
-            >
-            Regenerate
-            </button>
-          <Link 
-          href="code"
-          className='rounded-3xl py-4 px-12 text-2xl bg-green-500 text-white items-center flex justify-center'
+        <div className='fixed bottom-8 right-12 w-fit flex-col items-end justify-end flex gap-4'>
+          <button 
+          className='rounded-3xl py-4 text-2xl px-7 bg-red-500 text-white items-center flex justify-center'
+          >
+            Reject
+          </button>
+          <button 
+          className='rounded-3xl py-4 px-4 text-2xl bg-green-500 text-white items-center flex justify-center'
           >
             Approve
-          </Link>
+          </button>
         </div>
 
       </div>
+
+    </div>
   );
 }
